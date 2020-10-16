d8ca521361fb3ff5f942e4258b7ece3c9b675729
Erik Martines Sanches <erik.martinessanches@gmail.com>
2020-10-17 01:40:40 +0200

    Able to bundle own js and transpile Bootstrap's scss into css with modifiacations.

---
+++ CoreStart/client/js/index.js
@@ -0,0 +1,7 @@
+﻿import "../css/custom.scss";
+
+
+//TODO: Import bootstrap here. See https://getbootstrap.com/docs/4.5/getting-started/javascript/#individual-or-compiled
+
+
+console.log("Hello, cruel front-end world.");
---
+++ CoreStart/webpack.config.js
@@ -0,0 +1,31 @@
+﻿const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+    entry: './client/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/js/'),
+        filename: 'bundle.js'
+    },
+    plugins: [new MiniCssExtractPlugin({
+            filename: '../css/mainly.css'
+    })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+};
+
+module.exports = config;
