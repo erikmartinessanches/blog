a6edb8610556ad01569ac585bf87de8a321376f5
Erik Martines Sanches <erik.martinessanches@gmail.com>
2021-03-20 20:04:25 +0100

    Able to list blog posts of different categories on the index page.

--- CoreStart/Areas/Admin/Controllers/HomeController.cs
+++ CoreStart/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CoreStart.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -19,10 +21,13 @@ namespace CoreStart.Areas.Admin.Controllers
         /*Specifying IActionResult interface as the return type allows us to return any type of
          action result, for example a ViewResult.*/
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string id = "all")
         {
+            ViewBag.Categories = context.Categories.ToList();
+            ViewBag.SelectedCategory = id;
+            List<BlogPost> blogposts;
             /**Like the OrderBy() method, the Include() method doesn't execute at the database,
-             * instead it helps build up the query expression that the ToList() method eventually
+             * instead it helps build up the query expression that the ToListAsync() method eventually
              * executes.
              *
              * If we would only need the CategoryId value, not the data for the entire category
@@ -31,10 +36,24 @@ namespace CoreStart.Areas.Admin.Controllers
              * included when we select a BlogPost. However, here we want to get all the Category
              * data, hence we use the Include() method.
              */
-            var blogposts = await context.BlogPosts
-                .Include(m => m.Category)
-                .OrderByDescending(m => m.Time)
-                .ToListAsync();
+            if (id.Equals("all"))
+            {
+                blogposts = await context.BlogPosts
+                    .Include(m => m.Category)
+                    .Where(m => m.Time < DateTime.Now)
+                    .OrderByDescending(m => m.Time)
+                    .ToListAsync();
+            }
+            else
+            {
+                blogposts = await context.BlogPosts
+                    .Include(m => m.Category)
+                    .Where(m => m.Time < DateTime.Now)
+                    .Where(m => m.Category.Name == id)
+                    .OrderByDescending(m => m.Time)
+                    .ToListAsync();
+            }
+
             /*Returns a ViewResult object for the view associated with the action method.
              *ViewResult is a type of IActionResult. */
             return View(blogposts);
--- CoreStart/Controllers/HomeController.cs
+++ CoreStart/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CoreStart.Models;
@@ -20,8 +21,11 @@ namespace CoreStart.Controllers
         /*Specifying IActionResult interface as the return type allows us to return any type of
          action result, for example a ViewResult.*/
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string id = "all")
         {
+            ViewBag.Categories = context.Categories.ToList();
+            ViewBag.SelectedCategory = id;
+            List<BlogPost> blogposts;
             /**Like the OrderBy() method, the Include() method doesn't execute at the database,
              * instead it helps build up the query expression that the ToListAsync() method eventually
              * executes.
@@ -32,11 +36,24 @@ namespace CoreStart.Controllers
              * included when we select a BlogPost. However, here we want to get all the Category
              * data, hence we use the Include() method.
              */
-            var blogposts = await context.BlogPosts
-                .Include(m => m.Category)
-                .Where(m => m.Time < DateTime.Now)
-                .OrderByDescending(m => m.Time)
-                .ToListAsync();
+            if (id.Equals("all"))
+            {
+                blogposts = await context.BlogPosts
+                    .Include(m => m.Category)
+                    .Where(m => m.Time < DateTime.Now)
+                    .OrderByDescending(m => m.Time)
+                    .ToListAsync();
+            }
+            else
+            {
+                blogposts = await context.BlogPosts
+                    .Include(m => m.Category)
+                    .Where(m => m.Time < DateTime.Now)
+                    .Where(m => m.Category.Name == id)
+                    .OrderByDescending(m => m.Time)
+                    .ToListAsync();
+            }
+
             /*Returns a ViewResult object for the view associated with the action method.
              *ViewResult is a type of IActionResult. */
             return View(blogposts);
